6597dc6e8b013f939b3e6175357cc8c3f487b7c9
Joshua Nafziger <joshua@nafzigers.us>
2024-03-22 11:45:47 -0400

    Delete Button Press directory

--- Button Press/restart.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def restart_now():
-    # Command to restart the Raspberry Pi
-    restart_command = ["sudo", "reboot"]
-
-    # Execute the restart command
-    subprocess.run(restart_command)
