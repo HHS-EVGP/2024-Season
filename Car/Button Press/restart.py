acf52d33bc59ad888bb4e24092d9265a4038f5d3
Joshua Nafziger <joshua@nafzigers.us>
2024-03-23 11:00:45 -0400

    Fixed restart

--- Car/Button Press/restart.py
+++ Car/Button Press/restart.py
@@ -5,4 +5,7 @@ def restart_now():
     restart_command = ["sudo", "reboot"]
 
     # Execute the restart command
-    subprocess.run(restart_command)
+    subprocess.run(restart_command, check=True)
+
+if __name__ == "__main__":
+    restart_now()
