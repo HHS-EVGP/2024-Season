240693872a8ec70c1acba7d788a0b6122705e651
Joshua Nafziger <joshua@nafzigers.us>
2024-03-28 17:03:59 -0400

    Made Odometer

--- Base Station/Front End/main.js
+++ Base Station/Front End/main.js
@@ -14,52 +14,73 @@ updateContent();
 
 var realWindowsWidth, realWindowsHeight
 
-class gauge {
-    constructor(x, y, title) {
-        this.x = x;
-        this.y = y;
+class odometer {
+    constructor(x,y,size,title) {
+        this.x = size;
+        this.y = size;
+        this.size = size;
         this.title = title;
     }
-    drw(value) {
+    draw(value) {
+        strokeWeight(0);
+
+        fill('#5D6D7E');
+
+        circle(this.x, this.y, this.size);
 
         this.value = value;
 
         fill('#D6DBDF');    
-        arc(this.x, this.y, 200, 200, 5*QUARTER_PI, -QUARTER_PI);
+        arc(this.x, this.y, this.size, this.size, PI - QUARTER_PI , QUARTER_PI);
 
-        fill('#17202A');    
-        arc(this.x, this.y, 200, 200, 5*QUARTER_PI, -QUARTER_PI - (HALF_PI - HALF_PI * this.value/100));
-
-        fill('#5D6D7E');    
-        arc(this.x, this.y, 180, 180, -PI, 0);
-
+        fill('#17202A');         //  TODO THIS PART::  -QUARTER_PI - (HALF_PI - HALF_PI * this.value/100)
+        arc(this.x, this.y, this.size, this.size, PI - QUARTER_PI, PI - QUARTER_PI + ((5.4*this.value)*PI/180));
+        
         textAlign(CENTER);
+        
+        fill('#5D6D7E'); 
+        circle(this.x, this.y, this.size*0.9)
 
-        textSize(15);
+        textSize(this.size*0.075);
         fill('#17202A'); 
-        text( this.value + '%', this.x, this.y - 20);
-        textSize(25);
-        text( this.title, this.x, this.y+10);
+        text( this.value + 'mph', this.x, this.y - this.size*0.1);
+        textSize(this.size*0.125);
+        text( this.title, this.x, this.y+this.size*0.05);
+
+        textSize(this.size*0.075);
+        fill('white');
+        text("0", this.x+this.size*(-0.265165042945),  this.y+this.size*(0.265165042945), );
+        text("5", this.x+this.size*(-0.356646193611),  this.y+this.size*(0.115881372891), );
+        text("10", this.x+this.size*(-0.370383127723), this.y+this.size*(-0.0586629243901), );
+        text("15", this.x+this.size*(-0.303381372891), this.y+this.size*(-0.22041946961), );
+        text("20", this.x+this.size*(-0.170246437402), this.y+this.size*(-0.334127446571), );
+        text("25", this.x+this.size*(0.0),             this.y+this.size*(-0.375), );
+        text("30", this.x+this.size*(0.170246437402),  this.y+this.size*(-0.334127446571), );
+        text("35", this.x+this.size*(0.303381372891),  this.y+this.size*(-0.22041946961), );
+
+        fill('red');
+        text("40", this.x+this.size*(0.370383127723),  this.y+this.size*(-0.0586629243901));
+        text("45", this.x+this.size*(0.356646193611),  this.y+this.size*(0.115881372891));
+        text("50", this.x+this.size*(0.265165042945),  this.y+this.size*(0.265165042945));
 
         fill('#C0392B');
 
-        let angle = -QUARTER_PI + (HALF_PI * this.value/100);
+        let angle = (-135 + 5.4*this.value)*PI/180
 
-        let x = this.x + 90 * sin(angle);
-        let y = this.y - 90 * cos(angle);
+        let x = this.x + this.size*0.45 * sin(angle);
+        let y = this.y - this.size*0.45 * cos(angle);
 
-        let y1 = this.y - 60 * cos(angle) + 5  * sin(angle);
-        let x1 = this.x + 60 * sin(angle) + 5  * cos(angle);
+        let y1 = this.y - this.size*0.3 * cos(angle) + this.size*0.025 * sin(angle);
+        let x1 = this.x + this.size*0.3 * sin(angle) + this.size*0.025 * cos(angle);
 
-        let y2 = this.y - 60 * cos(angle) - 5  * sin(angle);
-        let x2 = this.x + 60 * sin(angle) - 5  * cos(angle);
+        let y2 = this.y - this.size*0.3 * cos(angle) - this.size*0.025 * sin(angle);
+        let x2 = this.x + this.size*0.3 * sin(angle) - this.size*0.025 * cos(angle);
 
         triangle(x, y, x1, y1, x2, y2);
     }
 }
 
-let g_one = new gauge(200,200, 'first');
-let g_two = new gauge(400,200, 'second');
+let speed_gauge = new odometer(500,500,200,'Speed');
 
 function setup() {
     realWindowsWidth = windowWidth - 184
@@ -69,26 +90,16 @@ function setup() {
 
 
     frameRate(20);
-    noStroke();
-
-    g_one.drw(60);
-    g_two.drw(20);
-
 }
+
 function windowResized() {
     realWindowsWidth = windowWidth - 184
     realWindowsHeight = windowHeight - 104
     resizeCanvas(windowWidth-184, windowHeight-104);
-  }
-let time = 0;
+}
+
 
 function draw() {
-    time += 1;
-
-
-    background('#5D6D7E');
-    g_one.drw(time%100);
-    g_two.drw(100 - time%100);
-    console.log(time)
+    speed_gauge.draw(50);
 }
 
