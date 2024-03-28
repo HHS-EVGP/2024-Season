881cc74b1700cc8b6676a88fde8975c1ef6fb0b7
Joshua Nafziger <joshua@nafzigers.us>
2024-03-28 15:44:57 -0400

    Removed p5.play

--- Base Station/Front End/main.js
+++ Base Station/Front End/main.js
@@ -14,19 +14,81 @@ updateContent();
 
 var realWindowsWidth, realWindowsHeight
 
+class gauge {
+    constructor(x, y, title) {
+        this.x = x;
+        this.y = y;
+        this.title = title;
+    }
+    drw(value) {
+
+        this.value = value;
+
+        fill('#D6DBDF');    
+        arc(this.x, this.y, 200, 200, 5*QUARTER_PI, -QUARTER_PI);
+
+        fill('#17202A');    
+        arc(this.x, this.y, 200, 200, 5*QUARTER_PI, -QUARTER_PI - (HALF_PI - HALF_PI * this.value/100));
+
+        fill('#5D6D7E');    
+        arc(this.x, this.y, 180, 180, -PI, 0);
+
+        textAlign(CENTER);
+
+        textSize(15);
+        fill('#17202A'); 
+        text( this.value + '%', this.x, this.y - 20);
+        textSize(25);
+        text( this.title, this.x, this.y+10);
+
+        fill('#C0392B');
+
+        let angle = -QUARTER_PI + (HALF_PI * this.value/100);
+
+        let x = this.x + 90 * sin(angle);
+        let y = this.y - 90 * cos(angle);
+
+        let y1 = this.y - 60 * cos(angle) + 5  * sin(angle);
+        let x1 = this.x + 60 * sin(angle) + 5  * cos(angle);
+
+        let y2 = this.y - 60 * cos(angle) - 5  * sin(angle);
+        let x2 = this.x + 60 * sin(angle) - 5  * cos(angle);
+
+        triangle(x, y, x1, y1, x2, y2);
+    }
+}
+
+let g_one = new gauge(200,200, 'first');
+let g_two = new gauge(400,200, 'second');
+
 function setup() {
     realWindowsWidth = windowWidth - 184
     realWindowsHeight = windowHeight - 104
     var canvas = createCanvas(windowWidth-184, windowHeight-104);
-    canvas.parent('p5Canvas'); // Make sure this ID matches the one in your div
+    canvas.parent('p5Canvas');
+
+
+    frameRate(20);
+    noStroke();
+
+    g_one.drw(60);
+    g_two.drw(20);
+
 }
 function windowResized() {
     realWindowsWidth = windowWidth - 184
     realWindowsHeight = windowHeight - 104
     resizeCanvas(windowWidth-184, windowHeight-104);
   }
-  
+let time = 0;
+
 function draw() {
-    background(220);
+    time += 1;
+
+
+    background('#5D6D7E');
+    g_one.drw(time%100);
+    g_two.drw(100 - time%100);
+    console.log(time)
 }
 
