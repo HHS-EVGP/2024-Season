82eca21fab43fe123e3d6dd8bdef7aa3a6a51cec
Joshua Nafziger <joshua@nafzigers.us>
2024-04-01 17:28:28 -0400

    Graphs~

--- Base Station/Front End/main.js
+++ Base Station/Front End/main.js
@@ -139,8 +139,9 @@ class odometer {
         }
     }
     hide(){
+        strokeWeight(0);
         fill('white'); 
-        circle(this.x, this.y, this.size);
+        circle(this.x, this.y, this.size*1.01);
     }
 }
 
@@ -264,22 +265,127 @@ class odometer_counter {
         );
     }
     hide(){
+        strokeWeight(0);
         fill("white");
         rect(
-            this.x-this.size,
-            this.y-(this.size/2.8),
-            this.size*2,
-            this.size*0.61
+            this.x-this.size*1.01,
+            this.y-this.size/2.8-1,
+            this.size*2.02,
+            this.size*0.615+1
         );
     }
 }
 
 class graph {
-    constructor(x,y,title,size=1) {
+    constructor(x,y,title,size=1,x_axis_title="Counter",y_axis_title="Value",unit="") {
         this.x = x;            // X Location
-        this.y = y;            // Rate (Relates to the value)
-        this.size = size*200;  // Size of Odometer
-        this.title = title;    // Name of Odometer
+        this.y = y;            // Y Location
+        this.size = size*200;
+        this.title = title;
+        this.x_title = x_axis_title;
+        this.y_title = y_axis_title;
+        this.unit = unit;
+    }
+    draw(x_values,y_values){
+        setup();
+        /* 
+        From:
+        this.x-this.size/2+(1/8)*this.size
+
+        To:
+        this.x+this.size/2-(6*this.size/200)
+
+        */
+
+        
+        /* 
+        From:
+        this.y+(18*this.size/200)
+
+        To:
+        this.y+this.size*(3/4)*(7/8)-(6*this.size/200)
+
+        */
+        let x_leng = x_values.length;
+        let y_min = min(y_values);
+        let y_max = max(y_values);
+
+        console.log(y_min)
+
+        for (let i = 0; i < x_leng; i++) {
+            let x_point = (this.x-this.size/2+(1/8)*this.size) + (((i+0.5)/x_leng)*((this.x+this.size/2-(6*this.size/200))-(this.x-this.size/2+(1/8)*this.size)));
+            let y_point = (this.y+(18*this.size/200)) + (i/10*((this.y+this.size*(3/4)*(7/8)-(6*this.size/200))-(this.y+(18*this.size/200))));
+            //X
+            text(
+                x_values[i],
+                x_point,
+                this.y+this.size*(3/4)*(7/8)+(6*this.size/200)
+            );
+            //Y  WORKING HERE
+            text(
+                y_min+(y_max-y_min)*(10-(i+1))/11,
+                this.x-this.size/2+(1/8)*this.size-(6*this.size/200),
+                y_point
+            );
+            strokeWeight(10);
+            point(
+                x_point,
+                y_values[i]+500
+            );
+        }
+    }
+    setup(){
+        strokeWeight(0);
+        fill("#dbdbdb");
+        rect(this.x-this.size/2,this.y,this.size,this.size*3/4);
+        strokeWeight(1);
+        fill("black");
+        line(
+            this.x-this.size/2+(1/8)*this.size,
+            this.y+this.size*(3/4)*(7/8),
+            this.x+this.size/2,
+            this.y+this.size*(3/4)*(7/8)
+        );
+        line(
+            this.x-this.size/2+(1/8)*this.size,
+            this.y,
+            this.x-this.size/2+(1/8)*this.size,
+            this.y+this.size*(3/4)*(7/8)
+        );
+        
+        textSize(10*this.size/200);
+        text(
+            this.x_title,
+            this.x,
+            this.y+this.size*(3/4)-4*this.size/200
+        );
+        let list_of_y_axis = [];
+        for (let i = 0; i < this.y_title.length; i++) {
+            list_of_y_axis[i] = (this.y_title)[i];
+            if(this.y_title.length%2 == 1){
+                text(
+                    list_of_y_axis[i],
+                    this.x-this.size/2+5*this.size/200,
+                    this.y+this.size*(3/4)*(1/2)+(9*this.size/200)*(i-(this.y_title.length-1)/2)
+                );
+            }else{
+                text(
+                    list_of_y_axis[i],
+                    this.x-this.size/2+5*this.size/200,
+                    this.y+this.size*(3/4)*(1/2)+(9*this.size/200)*(i-(this.y_title.length)/2)
+                );
+            }
+        }
+        text(
+            this.title,
+            this.x,
+            this.y+6*this.size/200
+        );
+    }
+    hide(){
+        strokeWeight(0);
+        fill("white");
+        rect(this.x-this.size/2,this.y,this.size,this.size*3/4)
     }
 }
 
@@ -290,9 +396,16 @@ objects[2] = new odometer(75*5,100,'Current','amps',150,-20,0.7,6);
 objects[3] = new odometer(75,100,'Speed','mph',50,0,0.7);
 objects[4] = new odometer_counter(75*8,50,'Miles',0.7);
 
-let Motor_Temp = new odometer(75*2.75,300+150/2,'Motor Temp',"°F",150,32,0.7,4);
-let Battery_Temp_1 = new odometer(75,300,'Temp 1',"°F",120,32,0.7,4);
-let Battery_Temp_2 = new odometer(75,450,'Temp 2',"°F",120,32,0.7,4);
+objects[5] = new odometer(75*2.75,300+150/2,'Motor Temp',"°F",150,32,0.7,4);
+objects[6] = new odometer(75,300,'Temp 1',"°F",120,32,0.7,4);
+objects[7] = new odometer(75,450,'Temp 2',"°F",120,32,0.7,4);
+
+objects[8] = new graph(600,200,'Acl. X',1);
+objects[9] = new graph(825,200,'Acl. Y',1);
+objects[10] = new graph(1050,200,'Acl. Z',1);
+objects[11] = new graph(600,375,'Gry. X',1);
+objects[12] = new graph(825,375,'Gry. Y',1);
+objects[13] = new graph(1050,375,'Gry. Z',1);
 
 function setup() {
     realWindowsWidth = windowWidth - 184;
@@ -303,184 +416,42 @@ function setup() {
     textAlign(CENTER,CENTER);
     frameRate(20);
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 14; i++) {
         objects[i].setup();
     }
 
-    Motor_Temp.setup();
-    Battery_Temp_1.setup();
-    Battery_Temp_2.setup();
-
 }
 
-function windowResized() {
+function windowResized() {  
     realWindowsWidth = windowWidth - 184
     realWindowsHeight = windowHeight - 104
     resizeCanvas(windowWidth-184, windowHeight-104);
+
+    for (let i = 0; i < 14; i++) {
+        objects[i].setup();
+    }
 }
 
 function draw() {
+    for (let i = 0; i < 14; i++) {
+        // console.log(i, items.includes(Allitems[i]),hidden[i]);
+
+        if(items.includes(Allitems[i])){
+            if(hidden[i]){
+                hidden[i] = false;
+                objects[i].setup();
+            }
+            //Update the object if it is live HERE
+
+            objects[8].draw([1,2,3,4,5,6,7,8,9,10],[1,5,2,7,2,6,8,2,7,3])
 
 
-
-    for (let i = 0; i < 18; i++) {
-        // if(items.includes(Allitems[0])){
-        //     ampHrs.setup();
-        // }else{
-        //     if(!hidden[0]){
-        //         ampHrs.hide();
-        //         hidden[0] = true;
-        //     }
-        // }
+        }else{
+            if(!hidden[i]){
+                objects[i].hide();
+                hidden[i] = true;
+            }
+        }
     }
-
-
-
-
-    
-    // if(items.includes('ca_AmpHrs')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('ca_Voltage')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('ca_Current')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('ca_Speed')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('ca_Miles')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('motor_temp')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('Battery_1')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('Battery_2')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('IMU_Accel_x')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('IMU_Accel_y')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('IMU_Accel_z')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('IMU_Gyro_x')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('IMU_Gyro_y')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('IMU_Gyro_z')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('Brake_Pedal')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('throttle')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('counter')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
-    // if(items.includes('time')){
-    //     ampHrs.setup();
-    // }else{
-    //     if(!hidden[0]){
-    //         ampHrs.hide();
-    //         hidden[0] = true;
-    //     }
-    // }
 }
 
