981c9d1e2a192cdc97c851798cd7296fc1e0f9dd
Joshua Nafziger <joshua@nafzigers.us>
2024-04-11 23:48:04 -0400

    Finished!

--- Base Station/Front End/main.js
+++ Base Station/Front End/main.js
@@ -1,7 +1,18 @@
 let items = [];
-let Allitems = ['ca_AmpHrs', 'ca_Voltage', 'ca_Current', 'ca_Speed', 'ca_Miles', 'motor_temp', 'Battery_1', 'Battery_2', 'IMU_Accel_x', 'IMU_Accel_y', 'IMU_Accel_z', 'IMU_Gyro_x', 'IMU_Gyro_y', 'IMU_Gyro_z', 'Brake_Pedal', 'throttle', 'counter', 'time'];
+let Allitems = ['ca_AmpHrs', 'ca_Voltage', 'ca_Current', 'ca_Speed', 'ca_Miles',
+ 'motor_temp', 'Battery_1', 'Battery_2',
+  'IMU_Accel_x', 'IMU_Accel_y', 'IMU_Accel_z', 'IMU_Gyro_x', 'IMU_Gyro_y','IMU_Gyro_z',
+  'Brake_Pedal', 'throttle', 'counter', 'time'];
 var hidden = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
 var objects = [];
+var map_to = [
+    14,15,16,17,18,
+    3,4,5,
+    8,9,10,11,12,13,
+    6,7,
+    2,1
+]
+var lastTenEntries;
 
 function updateContent() {
     // Get the selected value from the dropdown
@@ -10,6 +21,22 @@ function updateContent() {
     if(document.getElementById("itemSelection").value == "All"){
         items = Allitems;
     }
+}
+
+
+async function fetchAndProcessCSV() {
+    fetch('/2.data.csv')
+        .then(response => response.text())
+        .then(text => {
+            const lines = text.trim().split('\n');
+            // Assuming there's no header row, adjust if there is one
+            const lastTenLines = lines.slice(-10);
+            lastTenEntries = lastTenLines.map(line => line.split(','));
+            // return lastTenEntries, lastTenEntries[9];
+            // console.log("lastTenEntries",lastTenEntries);
+            // console.log("lastEntrie",lastTenEntries[9]);
+        })
+        .catch(error => console.error('Error fetching CSV:', error));
 }
 
 updateContent();
@@ -45,6 +72,9 @@ function setup() {
         objects[i].setup();
     }
 }
+function map(value, minInput, maxInput, minOutput, maxOutput) {
+    return (value - minInput) * (maxOutput - minOutput) / (maxInput - minInput) + minOutput;
+}
 
 function windowResized() {  
     realWindowsWidth = windowWidth - 184;
@@ -54,24 +84,61 @@ function windowResized() {
         objects[i].setup();
     }
 }
+async function draw() {
 
-function draw() {
-    for (let i = 0; i < objects.length; i++) {
-        if(items.includes(Allitems[i])){
-            if(hidden[i]){
-                hidden[i] = false;
-                objects[i].setup();
-            }
-            //Update the object if it is live HERE
+    // console.log(lastTenEntries);
 
-            // objects[15].draw(0);
+    fetchAndProcessCSV();
 
-            
 
-        }else{
-            if(!hidden[i]){
-                objects[i].hide();
-                hidden[i] = true;
+    if (lastTenEntries != null){
+        for (let i = 0; i < objects.length; i++) {
+            if(items.includes(Allitems[i])){
+                if(hidden[i]){
+                    hidden[i] = false;
+                    objects[i].setup();
+                }
+                //Update the object if it is live HERE
+
+                if(i >= 0 && i <= 7){
+                    if(lastTenEntries[9][map_to[i]] != ""){
+                        objects[i].draw(lastTenEntries[9][map_to[i]]);
+                    }
+                }else if(i >= 8 && i <= 13){
+                    var x = [], y = [];
+                    for(let o = 0; o < 10; o++){
+                        if(lastTenEntries[o][i] != ""){
+                            y.push(parseInt(lastTenEntries[o][map_to[i]]));
+                        }else{
+                            y.push(null);
+                        }
+                        x.push(parseInt(lastTenEntries[o][2]));
+                    }
+                    objects[i].draw(x,y);
+                }else if(i == 14 || i == 15){
+                    if(lastTenEntries[9][map_to[i]] != ""){
+                        let min_input = 0
+                        let max_input = 10000
+
+                        let input = map(lastTenEntries[9][map_to[i]],min_input,max_input,0,1);
+                        if(input > 1){
+                            input = 1;
+                        }else if(input < 0){
+                            input = 0;
+                        }
+                        objects[i].draw(input);
+                    }else{
+                        objects[i].draw(0);
+                    }
+                }else if(i == 16 || i == 17){
+                    objects[i].draw(lastTenEntries[9][map_to[i]]);
+                }
+    
+            }else{
+                if(!hidden[i]){
+                    objects[i].hide();
+                    hidden[i] = true;
+                }
             }
         }
     }
